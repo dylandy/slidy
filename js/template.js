2b4dcbe3d843cd147dcce3abe5f2a2b1b517f332
Dylandy Chang <steven80919@gmail.com>
2014-12-21 23:53:46 +0800

    some changed

--- js/template.js
+++ js/template.js
@@ -1,3 +1,4 @@
+var current;
 var id_generator = function(){
   var i = 0;
   $('section').map(function(){
@@ -5,19 +6,31 @@ var id_generator = function(){
   });
 }
 
+var place_section = function( callback ){
+  for(var i = 0 ; i < $('section').size() ; i++){
+    $('section:eq('+i+')').css('left' , i* $(window).width() + "px");
+  }
+  callback();
+}
+
 var left_slide = function(){
   $(document).on("keydown" , function(e){
-    var current;
-    $('section').each(function(){
-      if($(this).css("left") == "0px"){
+    $('section').map(function(){
+      var position = $(this).position();
+      if(position.left === 0){
         current = $(this);
       }
     });
+    console.log(current.attr("id"));
     if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34 && current.hasClass("left-slide") && current.attr("id") != $('section:last').attr("id")){
       e.preventDefault();
+      console.log("to right");
+      console.log(current.attr("id") != $('section:last').attr("id"));
       current.css("left" , 0 - $(window).width());
       current.next().css("left" , 0);
     }else if(e.which == 33 || e.which == 37 || e.which == 38 && current.hasClass("left-slide") && current.attr("id") != $('section:first').attr("id")){
+      console.log("to left");
+      console.log(current.attr("id") != $('section:first').attr("id"));
       e.preventDefault();
       current.css("left" , 0 + $(window).width());
       current.prev().css("left" , 0);
@@ -25,16 +38,12 @@ var left_slide = function(){
   });
 }
 
-var place_section = function(){
-  for(var i = 0 ; i < $('section').size() ; i++){
-    $('section:eq('+i+')').css('left' , i* $(window).width());
-  }
-}
+
 
 jQuery(function($){
-  $('section').css('width' , $(window).width() + "px");
-  $('section:first').css("left" , 0);
-  id_generator();
-  place_section();
-  left_slide();
+  place_section(function(){
+    id_generator();
+    left_slide();
+  });
+  
 });
