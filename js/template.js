8de38d2a6e0dfd58a6a36b2879dd3a3246889e58
Dylandy Chang <steven80919@gmail.com>
2014-12-21 16:59:44 +0800

    remove slide body with pure section for each slide

--- js/template.js
+++ js/template.js
@@ -1,26 +1,40 @@
+var id_generator = function(){
+  var i = 0;
+  $('section').map(function(){
+    $(this).attr("id" , i+=1);
+  });
+}
+
 var left_slide = function(){
   $(document).on("keydown" , function(e){
-    var left = parseInt($('#slide-body').css("left"));
-    var window_width = $(window).width();
-    var slide_count = $('section').size();
-    if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
+    var current;
+    $('section').each(function(){
+      if($(this).css("left") == "0px"){
+        current = $(this);
+      }
+    });
+    if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34 && current.hasClass("left-slide") && current.attr("id") != $('section:last').attr("id")){
       e.preventDefault();
-      $('#slide-body').css("left" , left - window_width );
-    }else if(e.which == 33 || e.which == 37 || e.which == 38){
+      current.css("left" , 0 - $(window).width());
+      current.next().css("left" , 0);
+    }else if(e.which == 33 || e.which == 37 || e.which == 38 && current.hasClass("left-slide") && current.attr("id") != $('section:first').attr("id")){
       e.preventDefault();
-      $('#slide-body').css("left" , left + window_width);
+      current.css("left" , 0 + $(window).width());
+      current.prev().css("left" , 0);
     }
   });
 }
 
 var place_section = function(){
-  for(var i = 0 ; i < $('#slide-body').children().size() ; i++){
+  for(var i = 0 ; i < $('section').size() ; i++){
     $('section:eq('+i+')').css('left' , i* $(window).width());
   }
 }
 
 jQuery(function($){
   $('section').css('width' , $(window).width() + "px");
+  $('section:first').css("left" , 0);
+  id_generator();
   place_section();
   left_slide();
 });
