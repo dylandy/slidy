4df3e97bbf2825986405b93be5d64e79b409cd47
dylandy.chang <steven80919@gmail.com>
2014-12-22 12:22:03 +0800

    fixed overscrolling issue

--- js/template.js
+++ js/template.js
@@ -21,19 +21,27 @@ var left_slide = function(){
         current = $(this);
       }
     });
-    console.log(current.attr("id"));
-    if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34 && current.hasClass("left-slide") && current.attr("id") != $('section:last').attr("id")){
-      e.preventDefault();
-      console.log("to right");
-      console.log(current.attr("id") != $('section:last').attr("id"));
-      current.css("left" , 0 - $(window).width());
-      current.next().css("left" , 0);
-    }else if(e.which == 33 || e.which == 37 || e.which == 38 && current.hasClass("left-slide") && current.attr("id") != $('section:first').attr("id")){
-      console.log("to left");
-      console.log(current.attr("id") != $('section:first').attr("id"));
-      e.preventDefault();
-      current.css("left" , 0 + $(window).width());
-      current.prev().css("left" , 0);
+    console.log(current.attr('id'));
+    if( current.attr("id") === '1'){
+      if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
+        current.css("left" , 0 - $(window).width());
+        current.next().css("left" , 0);
+      }
+    }else if( current.attr("id") == $("section").size() ){
+      if(e.which == 33 || e.which == 37 || e.which == 38){
+        current.css("left" , 0 + $(window).width());
+        current.prev().css("left" , 0);
+      }
+    }else{
+      if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
+        e.preventDefault();
+        current.css("left" , 0 - $(window).width());
+        current.next().css("left" , 0);
+      }else if(e.which == 33 || e.which == 37 || e.which == 38){
+        e.preventDefault();
+        current.css("left" , 0 + $(window).width());
+        current.prev().css("left" , 0);
+      }
     }
   });
 }
