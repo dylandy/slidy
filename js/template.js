45a3c7674534375c2b1e9dcd79fcc8b1c73c291d
dylandy.chang <steven80919@gmail.com>
2014-12-23 13:00:01 +0800

    fixed different eyecatch animate couldn't controlled by class's issue

--- js/template.js
+++ js/template.js
@@ -21,26 +21,27 @@ var left_slide = function(){
         current = $(this);
       }
     });
-    console.log(current.attr('id'));
-    if( current.attr("id") === '1'){
-      if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
-        current.css("left" , 0 - $(window).width());
-        current.next().css("left" , 0);
-      }
-    }else if( current.attr("id") == $("section").size() ){
-      if(e.which == 33 || e.which == 37 || e.which == 38){
-        current.css("left" , 0 + $(window).width());
-        current.prev().css("left" , 0);
-      }
-    }else{
-      if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
-        e.preventDefault();
-        current.css("left" , 0 - $(window).width());
-        current.next().css("left" , 0);
-      }else if(e.which == 33 || e.which == 37 || e.which == 38){
-        e.preventDefault();
-        current.css("left" , 0 + $(window).width());
-        current.prev().css("left" , 0);
+    if( current.hasClass("left-slide") ){
+      if( current.attr("id") === '1'){
+        if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
+          current.css("left" , 0 - $(window).width());
+          current.next().css("left" , 0);
+        }
+      }else if( current.attr("id") == $("section").size() ){
+        if(e.which == 33 || e.which == 37 || e.which == 38){
+          current.css("left" , 0 + $(window).width());
+          current.prev().css("left" , 0);
+        }
+      }else{
+        if(e.which == 13 || e.which == 32 || e.which == 39 || e.which == 40 || e.which ==34){
+          e.preventDefault();
+          current.css("left" , 0 - $(window).width());
+          current.next().css("left" , 0);
+        }else if(e.which == 33 || e.which == 37 || e.which == 38){
+          e.preventDefault();
+          current.css("left" , 0 + $(window).width());
+          current.prev().css("left" , 0);
+        }
       }
     }
   });
